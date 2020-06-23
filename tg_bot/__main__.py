c691b7620b954b0b753d2dceda7b6b8418842905
Nitin <52646268+Nitin1818@users.noreply.github.com>
2020-06-23 10:59:13 +0530

    Update __main__.py
    Signed-off-by: Nitin1818 <niksyead@gmail.com>

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -17,10 +17,10 @@ from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = f"""
-Hola there my name is *{dispatcher.bot.first_name}*. If you have any questions on how to use me, click help button.
+Hola {} my name is *{dispatcher.bot.first_name}*. If you have any questions on how to use me, click help button.
 
-I'm a group manager bot built in python3, using the python-telegram-bot library, and am fully opensource; \
-you can find the list of available commands with /help.
+I'm a group manager bot built in python3, using the python-telegram-bot v12 latest lib, and am fully opensource; \
+[Repo Source code](https://github.com/Nitin1818/Sakiyabot) Contribution is allowed.
 """
 buttons = [[InlineKeyboardButton(text="Help & Commands ❔", callback_data="help_back")]]
 
@@ -140,9 +140,9 @@ def start(update, context):
                 IMPORTED["rules"].send_rules(update, args[0], from_pm=True)
 
         else:
-            update.effective_message.reply_text(PM_START_TEXT, reply_markup=InlineKeyboardMarkup(buttons), parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True)
+            update.effective_message.reply_text(PM_START_TEXT.format(escape_markdown(first_name), reply_markup=InlineKeyboardMarkup(buttons), parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True)
     else:
-        update.effective_message.reply_text("Yo, whadup?")
+        update.effective_message.reply_text("How can i help u?")
 
 
 # for test purposes
